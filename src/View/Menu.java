9d52575537dcc0bf8c9ffbfc5b6f0e598a6e49b1
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2019-06-08 01:29:06 +0100

    wwerwer

--- src/View/Menu.java
+++ src/View/Menu.java
@@ -254,6 +254,15 @@ public class Menu implements IMenu{
         new Scanner(System.in).nextLine();
     }
 
+    public void showSave(String fName, String time){
+        this.displayMenuHeader(time);
+        out.println();
+        out.println("Estado guardado com sucesso!");
+        out.println(fName);
+
+        new Scanner(System.in).nextLine();
+    }
+
     private <T> Table defaultTable(List <String> colLabl, List<List <T>> vals){
         List<String> linLabl = new ArrayList<>();
         for(int i = 0; i < vals.size(); i++)
